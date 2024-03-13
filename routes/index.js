d305907e1fee7810336fc3102c6361cf8d2b3348
Rita2024 <ritaejezie2@gmail.com>
2024-03-13 14:48:15 -0700

    fix index

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,29 @@
-// eslint-disable-next-line no-unused-vars
-import { Express } from 'express';
-import AppController from '../controllers/AppController';
-import AuthController from '../controllers/AuthController';
-import UsersController from '../controllers/UsersController';
-import FilesController from '../controllers/FilesController';
-import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
-import { APIError, errorResponse } from '../middlewares/error';
+const express = require('express');
 
-/**
- * Injects routes with their handlers to the given Express application.
- * @param {Express} api
- */
-const injectRoutes = (api) => {
-    api.get('/status', AppController.getStatus);
-    api.get('/stats', AppController.getStats);
+// create a new express route
+const route = express.Router();
 
-    api.get('/connect', basicAuthenticate, AuthController.getConnect);
-    api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+// import the controllers
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
-    api.post('/users', UsersController.postNew);
-    api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
+// endpoint to check the status of the API
+route.get('/status', AppController.getStatus);
+route.get('/stats', AppController.getStats);
 
-    api.post('/files', xTokenAuthenticate, FilesController.postUpload);
-    api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
-    api.get('/files', xTokenAuthenticate, FilesController.getIndex);
-    api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
-    api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
-    api.get('/files/:id/data', FilesController.getFile);
+// endpoint to create a new user
+route.post('/users', UsersController.postNew);
 
-    api.all('*', (req, res, next) => {
-        errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
-    });
-    api.use(errorResponse);
-};
+// endpoint to log a user in and out
+route.get('/connect', AuthController.getConnect);
+route.get('/disconnect', AuthController.getDisconnect);
+route.get('/users/me', AuthController.getMe);
 
-export default injectRoutes;
+// endpoint to create a new file
+route.post('/files', FilesController.postUpload);
+route.get('/files/:id', FilesController.getShow);
+route.get('/files', FilesController.getIndex);
+
+module.exports = route;
